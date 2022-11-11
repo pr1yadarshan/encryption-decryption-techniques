426cf6ca6ed744d143f56c05a849d9ad954948e6
Priyadarshan <developer.priyadarshan@gmail.com>
2022-11-12 00:58:53 +0530

    initial spring-boot project push

---
+++ src/main/java/com/priyadarshan/EncryptionDecryptionTechniquesApplication.java
@@ -0,0 +1,13 @@
+package com.priyadarshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionDecryptionTechniquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptionDecryptionTechniquesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyadarshan/EncryptionDecryptionTechniquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyadarshan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptionDecryptionTechniquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
